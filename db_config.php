108f63e87ab63d35b313841948741e69aaf98d83
Ezra Natanael <ezrantn.dev@gmail.com>
2024-09-25 10:41:19 +0700

    add prettier

--- db_config.php
+++ db_config.php
@@ -6,10 +6,8 @@ $username = $env["DB_USERNAME"];
 $password = $env["DB_PASSWORD"];       
 $dbname = $env["DB_NAME"];    
         
-    // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
         
-    // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
